664e766b4abd4df6998f7d61a3e930b808c5bf2b
samias9 <115688045+samias9@users.noreply.github.com>
2024-12-09 01:59:00 -0500

    ++ server.js

--- nextchapter/backend/server.js
+++ nextchapter/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Importation de cors
 require('dotenv').config();
+require('dotenv').config({ path: './backend/.env' });
 
 const app = express();
 
@@ -12,10 +13,15 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
 }));
 
+//j'ai ajouté app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
 const userRoutes = require('./routes/userRoute');
+const bookRoutes = require('./routes/bookRoutes');
+
 app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
 
 // Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -25,3 +31,12 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(5000, () => {
   console.log("Serveur démarré sur le port 5000");
 });
+/*
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.error("Erreur de connexion à MongoDB :", error));
+
+  const PORT = process.env.PORT || 5002;
+
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+*/
